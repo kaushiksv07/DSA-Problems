4d5398f536080b3e2c6e2dfff3cd67bc40eb17c8
Kaushik S V <kaushik.sv01@gmail.com>
2025-01-04 14:36:11 +0530

    folder change

---
+++ DSA-Problems/Arrays/KadanesAlgo.java
@@ -0,0 +1,11 @@
+/*
+Kadans algo - find maximum sum of subarray in array
+
+A[] =  5 6 7 3 2 10 12 8 12 21 4 7
+Sfm =  5 11 18 15 17 7 -5/0 8 20 41 37 44
+Max =  5 11 18 18 18 18 18 18 20 41 41 44
+
+
+
+*/
+
